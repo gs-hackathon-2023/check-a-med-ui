22ef8beca7bc11e5b3ae8071c9305fa5c8cb4ccb
boris.katzman <borisk58@gmail.com>
2023-02-01 16:23:32 +0200

    login form

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Google from "../assets/google.svg";
+import Logo from "../assets/logo.svg";
 import Link from "next/link";
+
+
 export default function Home() {
   return (
     <div
@@ -8,29 +11,61 @@ export default function Home() {
         "h-screen bg-blue-50 w-screen flex justify-center items-center"
       }
     >
-      <div className="flex bg-gradient-to-r p-6 from-green-100 to-blue-200 items-center justify-around flex-col h-3/5 w-2/5 shadow-xl rounded-xl">
-        <h1 className="text-7xl font-medium mb-4">CHECKaMED</h1>
-        <h3 className="font-medium">
-          Get peace of mind with our innovative medication management tool. Stay
-          informed of all potential side effects and conflicts with your current
-          medicines, and never miss a new update with personalized
-          notifications.
+      <div className="flex bg-gradient-to-r p-6 from-green-100 to-blue-200 items-center justify-around flex-col h-5/5 w-2/5 shadow-xl rounded-xl space-y-4">
+        <Logo />
+
+       <h3 className="flex font-medium text-center" >
+          Eliminating dangerous interactions, one pill at a time. <br></br>
+          Stay informed about all potential side effects <br></br> and negative drug interactions 
+          between your current medicines. 
+          <br></br>Never miss an update through our personal notifications.
+          
         </h3>
-        <Link href={"/dashboard"}>
-          <button
-            className="bg-green-500 p-3 rounded-lg hover:bg-green-400 "
-            type="submit"
-          >
-            <span
-              className={
-                "flex text-white items-center justify-center space-x-3 text-center"
-              }
+        <form className="bg-white p-6 rounded-lg shadow-md ">
+          <h2 className="text-lg font-medium mb-4">Please Sign In:</h2>
+          <div className="mb-4 " >
+            <label className="block text-gray-700 font-medium mb-2" >
+              User ID
+            </label>
+            <input
+                className="w-full p-3 bg-gray-200 rounded-lg"
+                type="text"
+                id="userId"
+                name="userId"
+            />
+          </div>
+          <div className="mb-6">
+            <label className="block text-gray-700 font-medium mb-2" htmlFor="password">
+                Password
+            </label>
+            <input
+                className="w-full p-3 bg-gray-200 rounded-lg"
+                type="password"
+                id="password"
+                name="password"
+            />
+          </div>
+          <div className="flex items-center justify-between space-x-2 space-y-2">
+            <button
+                className="bg-blue-600 text-white p-3 rounded-lg hover:bg-blue-500"
+                type="submit"
             >
-              <div>Sign in with</div>
-              <Google />
-            </span>
-          </button>
-        </Link>
+                <div>Sign In</div>
+            </button>
+            <a className="text-blue-600 hover:text-blue-500" href="#">
+                Forgot password?
+            </a>
+            <Link href={"/dashboard"}>
+            <button className="bg-blue-600 text-white p-3 rounded-lg hover:bg-blue-500 ">
+              <span className={"flex  items-center justify-center space-x-3 text-center"}>
+                <div>Sign in with</div>
+                <Google />
+              </span>
+            </button>
+          </Link>
+            
+          </div>
+        </form>
       </div>
     </div>
   );
